5551d71b6d03c850cccc7d3944481cbc7db26af3
gis3ll3-m <g_marentes@uncg.edu>
2026-03-06 14:44:45 -0500

    added file for @Repository interface.

---
+++ a3/src/main/java/com/example/a3/CharactersRepository.java
@@ -0,0 +1,12 @@
+package com.example.a3;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CharactersRepository extends JpaRepository<Characters, Long>{
+    List<Characters> findByBellyBadge(String bellyBadge);
+    List<Characters> findBYNameContaining(String name);
+}
